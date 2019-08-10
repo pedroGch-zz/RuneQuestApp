bf5a2882a0decd46f9f35ed58191a2e446f140d4
pedro <pedro.gch01@gmail.com>
2019-08-09 23:49:10 -0300

    comienso de la maquetacion de formulario alta personaje

--- public/js/armas.js
+++ public/js/armas.js
@@ -5,22 +5,6 @@
   
   });
 
-  $('.datepicker').pickadate({
-    min: new Date(),
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year
-    firstDay: 1
-  });
-
-var picker = $('#beginDate').pickadate('picker');
-picker.set('select', new Date());
-var picker = $('#endDate').pickadate('picker');
-picker.set('select', new Date());
-
-
-$('select').material_select();
-
-
 var form = $("#example-form");
 form.children("div").steps({
     headerTag: "h3",
